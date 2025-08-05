a6f50571fd64f2826b4c5e5d772f0b978272988c
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2025-08-05 13:24:38 -0400

    Get restaurant data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+
+import {useState,useEffect} from "react"
 import Header from "./Components/Header"
 import RestaurantCard from "./Components/RestaurantCard"
 
 
+
 function App() {
 
-  const restaurantData = {
-  
-  name:"Pizza Hut",
-  cusine: ["Pizza","salad"],
-  star: "4.6",
-  price: "Items At ₹199",
-  deliveryTime: "15 to 26 Minutes"
+  const[restaurantData,setRestaurantData] = useState([])
 
+  useEffect(()=>{
   
-  }
+    fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    .then(res => res.json())
+    .then (data => setRestaurantData(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants))
+  },[])
+
 
 
   return (
@@ -21,9 +23,11 @@ function App() {
     <>
     <Header/>
 
-    <div className="mx-20 my-20">
-      <h1 className="text-2xl text-white mb-12">Top Restaurant Chains In Ahmedabad</h1>
-      <RestaurantCard data={restaurantData} />
+    <h1 className="text-2xl text-white mb-12">Top Restaurant Chains In Ahmedabad</h1>
+    <div className="mx-20 my-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+
+   
+      {restaurantData.map((restaurant) =><RestaurantCard key={restaurant.info.id} data={restaurant} /> )}
     </div>
      
     </>
--- src/Components/RestaurantCard.jsx
+++ src/Components/RestaurantCard.jsx
@@ -1,49 +1,56 @@
-import { MdStars,MdDeliveryDining } from "react-icons/md";
-
-
-
+import { MdStars, MdDeliveryDining } from "react-icons/md";
 
 const RestaurantCard = (props) => {
 
-    const {name,cusine,star,price,deliveryTime}=props.data
-    return (
-      <div className="flex flex-col w-80 text-white cursor-pointer hover:scale-95 duration-120">
-        <div className="max-w-sm rounded-xl overflow-hidden shadow-lg">
+  const{name,avgRating,sla,cloudinaryImageId,aggregatedDiscountInfoV3,cuisines} = props.data.info
+  const offer = aggregatedDiscountInfoV3.header +" "+ aggregatedDiscountInfoV3.subHeader
+  const IMG_URL =  import.meta.env.VITE_IMG_URL;
+
+
+  return (
+
+    <div className="relative group w-80">
+      {/* Top border animation */}
+      <span className="absolute top-0 left-0 h-[2px] bg-orange-500 w-0 group-hover:w-full transition-all duration-500"></span>
+
+      {/* Left border animation */}
+      <span className="absolute top-0 left-0 w-[2px] bg-orange-500 h-0 group-hover:h-full transition-all duration-500"></span>
+
+      <div className="flex flex-col text-white cursor-pointer hover:scale-95 transition-transform duration-300">
+        <div className="max-w-sm w-80 rounded-xl overflow-hidden shadow-lg">
           <div className="relative">
             <img
               className="w-full h-54 object-cover shadow-xl"
-              src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/6/9/468d7ec1-400c-45e1-8fe2-67237a3174cf_47589.JPG"
+              src={IMG_URL + cloudinaryImageId }
               alt="Restaurant"
             />
-   
             <div className="absolute bottom-0 left-0 w-full bg-[#2D3250]/90 px-4 py-2">
-              <h2 className="text-lg font-bold text-white uppercase">{price}</h2>
+              <h2 className="text-lg font-bold text-white uppercase">{offer}</h2>
             </div>
           </div>
-  
-          <div className="px-6 py-4">
-            <h3 className="text-orange-500 font-bold text-xl">{name}</h3>
-           
-            <div className="flex flex-wrap gap-1 text-gray-500 text-base">
-             {cusine.join(", ")}
+
+          <div className="px-6 py-4 bg-[#1e1e2f]">
+            <h3 className="text-orange-500 font-bold text-xl capitalize">{name}</h3>
+
+            <div className="gap-2 text-gray-400 text-base">
+            {cuisines.slice(0, 3).join(", ")}{cuisines.length > 3 ? ", ..." : ""}
             </div>
-            
+
             <div className="flex items-center gap-2 text-center mt-2 mb-2">
-                <MdStars className="text-orange-500 size-5 " />
-                <span className="text-sm font-semibold mr-2">{star}</span>
-                <MdDeliveryDining className="text-orange-500 size-6" />
-                <span className="text-sm font-semibold mr-2">{deliveryTime}</span>
+              <MdStars className="text-orange-500 size-5" />
+              <span className="text-sm font-semibold mr-2">{avgRating}</span>
+              <MdDeliveryDining className="text-orange-500 size-6" />
+              <span className="text-sm font-semibold mr-2">{sla.deliveryTime} Minutes</span>
             </div>
+
             <button className="mt-4 cursor-pointer border border-orange-500 hover:bg-orange-500 text-white font-semibold text-sm px-4 py-2 rounded-full transition duration-300">
-                 Order Now
+              Order Now
             </button>
           </div>
-  
-          
         </div>
       </div>
-    );
-  };
-  
-  export default RestaurantCard;
-  
+    </div>
+  );
+};
+
+export default RestaurantCard;
